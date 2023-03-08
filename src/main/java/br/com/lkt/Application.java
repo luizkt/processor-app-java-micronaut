5074918cee9daedd20cdb4312bce9021d9d6ee03
luizkt <keese.luiz@gmail.com>
2023-03-08 14:33:13 -0300

    Primeiro commit

---
+++ src/main/java/br/com/lkt/Application.java
@@ -0,0 +1,18 @@
+package br.com.lkt;
+
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+            title = "processor-app-java-micronaut",
+            version = "0.0"
+    )
+)
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
